1fdcf3fd46a49bc451e1fac8cd8a9c9ce9722b0c
Jhon Tx <82347402+Jhon-Fs@users.noreply.github.com>
2024-12-31 23:22:13 -0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+window.addEventListener('keydown', function(e) {
+    if ((e.ctrlKey && (e.keyCode === 85)) || (e.ctrlKey && e.shiftKey && (e.keyCode === 73)) || (e.keyCode === 123) || (e.ctrlKey && e.shiftKey && (e.keyCode === 67))) {
+      e.preventDefault();
+    }
+});
+
+window.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
