8e8c78129d7f1374ae76ec01a7fd3225082c7b71
Jhon Tx <82347402+Jhon-Fs@users.noreply.github.com>
2025-02-18 22:58:23 -0300

    Delete JS directory

--- JS/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('keydown', function(e) {
-    if ((e.ctrlKey && (e.keyCode === 85)) || (e.ctrlKey && e.shiftKey && (e.keyCode === 73)) || (e.keyCode === 123) || (e.ctrlKey && e.shiftKey && (e.keyCode === 67))) {
-      e.preventDefault();
-    }
-});
-
-window.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-});
